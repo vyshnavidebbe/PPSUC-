b295958317e91b6a81ee8885ba73a1a26d388b08
vyshnavidebbe <vyshnavidebbe@gmail.com>
2025-12-03 19:08:54 +0530

    Add files via upload

---
+++ CLIMATE.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int C;
+    scanf("%d", &C);
+
+    if(C > 20) {
+        printf("HOT");
+    } else {
+        printf("COLD");
+    }
+
+    return 0;
+}
+
+
