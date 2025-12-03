0f2858a3bc6c93398772e28021bbb6477c0749e9
vyshnavidebbe <vyshnavidebbe@gmail.com>
2025-12-03 21:18:25 +0530

    Add files via upload

---
+++ addition of two numders.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Read two numbers from input
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate sum
+    int sum = num1 + num2;
+
+    // Print result
+    printf("%d", sum);
+
+    return 0;
+}
