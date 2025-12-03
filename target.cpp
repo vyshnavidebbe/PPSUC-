331a5561335977b25f54cbf079778db2349996f6
vyshnavidebbe <vyshnavidebbe@gmail.com>
2025-12-03 20:53:28 +0530

    Add files via upload

---
+++ target.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int P1, P2, P3, P4;
+    scanf("%d %d %d %d", &P1, &P2, &P3, &P4);
+
+    // Check if he solved at least 10 problems each week
+    if(P1 >= 10 && P2 >= 10 && P3 >= 10 && P4 >= 10) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+
+    return 0;
+}
+
