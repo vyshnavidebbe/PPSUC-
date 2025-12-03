f528f5376fb53ff2e691eb28f2fcc663b02cf177
vyshnavidebbe <vyshnavidebbe@gmail.com>
2025-12-03 20:29:04 +0530

    Add files via upload

---
+++ deck.cpp
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+int main() {
+    int A;
+    scanf("%d", &A);
+
+    if (A % 2 == 0 && A % 7 == 0) {
+        printf("Alice
+");
+    } else if (A % 2 == 1 && A % 9 == 0) {
+        printf("Bob
+");
+    } else {
+        printf("Charlie
+");
+    }
+
+    return 0;
+}
+
+
+
